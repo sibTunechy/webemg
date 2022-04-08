5e12a549e1e54d785eeb39f2f745d5125b69f3c2
sibTunechy <ayotemitope930@gmail.com>
2022-04-08 23:36:05 +0100

    new

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo>EMG</NavLogo>
+            </NavbarContainer>
+        </Nav>
+    </>
+  )
+}
+
+export default Navbar;
