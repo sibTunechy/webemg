6a59d3fdf77296a142c04374487c47992e124149
sibTunechy <ayotemitope930@gmail.com>
2022-04-11 13:29:49 +0100

    nav ready

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { NavbarContainer, NavLogo, Nav } from './NavbarElements';
+import React from 'react';
+import { AiOutlineBars } from 'react-icons/ai'
+import { NavbarContainer, NavLogo, Nav, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
 
 const Navbar = () => {
   return (
@@ -8,15 +9,30 @@ const Navbar = () => {
             <NavbarContainer>
                 <NavLogo to='/'>EMG</NavLogo>
                 <MobileIcon>
-                  <FaBars/>
+                  <AiOutlineBars/>
                 </MobileIcon>
                 <NavMenu>
                   <NavItem>
-                    <NavLinks to='about'>About</NavLinks>
+                    <NavLinks to='/'>Home</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='about'>About Us</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='services'>Services</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='contact'>Contact</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to='discover'>Discover</NavLinks>
                   </NavItem>
                 </NavMenu>
+                <NavBtn>
+                  <NavBtnLink to='/joinus'>Join Us</NavBtnLink>
+                </NavBtn> 
             </NavbarContainer>
-        </Nav>
+        </Nav> 
     </>
   )
 }
---
+++ src/components/Sidebar/SidebarElements.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { FaTimes } from 'react-icons/fa';
+
+export const SidebarContainer = styled.aside`
+    position: fixed;
+    z-index: 999;
+    width: 100%;
+    background: #0d0d0d;
+    display: grid;
+    align-items: center;
+    top: 0;
+    left: 0;
+    transition: 0.3s ease-in-out;
+`
