46ec61ba932d0fdbe4cf95d4a8efe9b915058609
sibTunechy <ayotemitope930@gmail.com>
2022-04-11 14:36:52 +0100

    nice

--- src/components/Sidebar/SidebarElements.jsx
+++ src/components/Sidebar/SidebarElements.jsx
@@ -11,4 +11,47 @@ export const SidebarContainer = styled.aside`
     top: 0;
     left: 0;
     transition: 0.3s ease-in-out;
+    opacity: ${({ isOpen }) => (isOpen ? '100%': '0')};
+    top: ${({ isOpen }) => ( isOpen ? '0' : '-100%')};
+`;
+
+export const CloseIcon = styled(FaTimes)`
+    color: #fff;
+`;
+
+export const Icon = styled.div`
+    position: absolute;
+    top: 1.2rem;
+    right: 1.5rem;
+    background: transparent;
+    font-size: 2rem;
+    cursor: pointer;
+    outline: none;
+`;
+
+export const SidebarWrapper = styled.div`
+    color: #fff;
+`;
+
+export const SidebarLink = styled(LinkScroll)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5rem;
+    text-decoration: none;
+    list-style: none;
+    transition: 0.2s ease-in-out;
+    text-decoration: none;
+    color: #fff;
+    cursor: pointer;
+
+    &:hover {
+        color: #01bf71;
+        transition: 0.2s ease-in-out;
+    }
+`;
+
+export const SideBtnwrap = styled.div`
+    display: flex;
+    justify-content: center;
 `
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { SidebarContainer, Icon, CloseIcon } from './SidebarElements';
 
 const Sidebar = () => {
   return (
@@ -6,6 +7,17 @@ const Sidebar = () => {
         <Icon>
             <CloseIcon/>
         </Icon>
+        <SidebarWrapper>
+            <SidebarMenu>
+                <SidebarLink to='/'>Home</SidebarLink>
+                <SidebarLink to='about'>About Us</SidebarLink>
+                <SidebarLink to='services'>Service</SidebarLink>
+                <SidebarLink to='discover'>Discover</SidebarLink>
+            </SidebarMenu>
+            <SideBtnWrap>
+                <SidebarRoute to='/joinus'>Join Us</SidebarRoute>
+            </SideBtnWrap>
+        </SidebarWrapper>
     </SidebarContainer>
   )
 }
