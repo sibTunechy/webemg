fb8827845b6759784047194ff96357e181d9037e
sibTunechy <ayotemitope930@gmail.com>
2022-04-12 16:39:21 +0100

    hero section

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState} from 'react';
+import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElements';
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements';
 
 const HeroSection = () => {
+
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
   return (
     <HeroContainer>
         <HeroBg>
             <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
         </HeroBg>
+        <HeroContent>
+            <HeroH1>Elizabeth Mattew Group</HeroH1>
+            <HeroP>
+                Sign up for a new account on EMG and receive
+            </HeroP>
+            <HeroBtnWrapper>
+                <Button to='joinus' onMouseEnter={onHover} onMouseLeave={onHover}>
+                    Get Started {hover ? <ArrowForward/> : <ArrowRight/>}
+                </Button>
+            </HeroBtnWrapper>
+        </HeroContent>
     </HeroContainer>
   )
 }
