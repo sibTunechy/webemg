e3ff914feef1a8a0e9efbebbec56696c5904067a
sibTunechy <ayotemitope930@gmail.com>
2022-04-16 21:16:57 +0100

    more

--- src/components/InfoSection/index.jsx
+++ src/components/InfoSection/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Button } from 'react-scroll';
+import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img } from './InfoElements';
 
 const InfoSection = () => {
   return (
     <>
-        <InfoContainer id={id}>
+        <InfoContainer >
             <InfoWrapper>
                 <InfoRow>
                     <Column1>
@@ -17,6 +18,11 @@ const InfoSection = () => {
                             </BtnWrap>
                         </TextWrapper>
                     </Column1>
+                    <Column2>
+                        <ImgWrap>
+                            <Img/>
+                        </ImgWrap>
+                    </Column2>
                 </InfoRow>
             </InfoWrapper>
         </InfoContainer>
