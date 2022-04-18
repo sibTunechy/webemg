00c885efcd1e5c62f5696e3941ad3965e98d67fe
sibTunechy <ayotemitope930@gmail.com>
2022-04-18 18:15:55 +0100

    more

---
+++ src/components/InfoSection/Data.jsx
@@ -0,0 +1,15 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headLine: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    primary: 'true',
+    darkText: false
+};
