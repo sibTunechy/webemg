4ae7dd58e3088f93c60cf178d885360f99f9cf50
sibTunechy <ayotemitope930@gmail.com>
2022-04-20 14:06:02 +0100

    more

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
+import JoinusPage from './pages/joinus';
 
 
 
 function App() {
     return (
         <Router>
-            <Home/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/joinus' element={<JoinusPage/>}/>
+            </Routes>
         </Router>
     )
 }
