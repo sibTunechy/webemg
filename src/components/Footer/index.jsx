328f3a6a7d38975d77e55785c8ab4ee85875d9e5
sibTunechy <ayotemitope930@gmail.com>
2022-04-21 15:45:43 +0100

    more

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,14 +7,38 @@ const Footer = () => {
             <FooterLinksContainer>
                 <FooterLinksWrapper>
                     <FooterLinkItems>
-                        <FooterLinkTitle>
-                            <FooterLink to='/about'>About Us</FooterLink>
+                        <FooterLinkTitle>About Us</FooterLinkTitle>
                             <FooterLink to='/about'>How it works</FooterLink>
                             <FooterLink to='/about'>Testimonials</FooterLink>
                             <FooterLink to='/about'>Careers</FooterLink>
                             <FooterLink to='/about'>Terms of Use</FooterLink>
-                            <FooterLink to='/about'>Privacy Policy</FooterLink>
-                        </FooterLinkTitle>
+                            <FooterLink to='/about'>Privacy Policy</FooterLink>                     
+                    </FooterLinkItems>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to='/about'>How it works</FooterLink>
+                            <FooterLink to='/about'>Testimonials</FooterLink>
+                            <FooterLink to='/about'>Careers</FooterLink>
+                            <FooterLink to='/about'>Terms of Use</FooterLink>
+                            <FooterLink to='/about'>Privacy Policy</FooterLink>                     
+                    </FooterLinkItems>
+                </FooterLinksWrapper>
+                <FooterLinksWrapper>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to='/about'>How it works</FooterLink>
+                            <FooterLink to='/about'>Testimonials</FooterLink>
+                            <FooterLink to='/about'>Careers</FooterLink>
+                            <FooterLink to='/about'>Terms of Use</FooterLink>
+                            <FooterLink to='/about'>Privacy Policy</FooterLink>                     
+                    </FooterLinkItems>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to='/about'>How it works</FooterLink>
+                            <FooterLink to='/about'>Testimonials</FooterLink>
+                            <FooterLink to='/about'>Careers</FooterLink>
+                            <FooterLink to='/about'>Terms of Use</FooterLink>
+                            <FooterLink to='/about'>Privacy Policy</FooterLink>                     
                     </FooterLinkItems>
                 </FooterLinksWrapper>
             </FooterLinksContainer>
