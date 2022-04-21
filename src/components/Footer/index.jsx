1b820206bd8e8cf62d88491554781e496ce1e0a8
sibTunechy <ayotemitope930@gmail.com>
2022-04-21 15:25:35 +0100

    more

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-
-    </div>
+    <FooterContainer>
+        <FooterWrap>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinkItems>
+                        <FooterLinkTitle>
+                            <FooterLink to='/about'>About Us</FooterLink>
+                            <FooterLink to='/about'>How it works</FooterLink>
+                            <FooterLink to='/about'>Testimonials</FooterLink>
+                            <FooterLink to='/about'>Careers</FooterLink>
+                            <FooterLink to='/about'>Terms of Use</FooterLink>
+                            <FooterLink to='/about'>Privacy Policy</FooterLink>
+                        </FooterLinkTitle>
+                    </FooterLinkItems>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
+        </FooterWrap>
+    </FooterContainer>
   )
 }
 
