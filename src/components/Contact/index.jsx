44b467b22ca7c2fca72965e4eb215f4df24d247a
sibTunechy <ayotemitope930@gmail.com>
2022-04-24 19:30:07 +0100

    more

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <ContactWrapper>
             <ContactH1>Contact Us</ContactH1>
                 <ContactH2>Redworth Terraces 2, Gbangbala Street Ikate Elegushi, Lagos</ContactH2>
-                {/* <ContactSpan>0700 EMG 7000 (0700 364 7000) </ContactSpan> */}
+                <ContactSpan>0700 EMG 7000 (0700 364 7000) </ContactSpan>
         </ContactWrapper>
     </>
   )
