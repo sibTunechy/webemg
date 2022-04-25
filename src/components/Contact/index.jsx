644ca5f69918cfee6847c2cbaeac4cbc745ea91c
sibTunechy <ayotemitope930@gmail.com>
2022-04-25 22:53:16 +0100

    morr

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BsFillTelephoneFill } from 'react-icons';
 
 const Contact = () => {
   return (
@@ -7,6 +8,7 @@ const Contact = () => {
             <ContactH1>Contact Us</ContactH1>
                 <ContactH2>Redworth Terraces 2, Gbangbala Street Ikate Elegushi, Lagos</ContactH2>
                 <ContactSpan>0700 EMG 7000 (0700 364 7000) </ContactSpan>
+                <ContactSpan2 <BsFillTelephoneFill/> > 234 903 989 8796</ContactSpan2>
         </ContactWrapper>
     </>
   )
---
+++ src/components/More/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const More = () => {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default More;
