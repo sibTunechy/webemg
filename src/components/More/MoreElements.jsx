44049218f24388e60a59033fe00a88cce4aa5c86
sibTunechy <ayotemitope930@gmail.com>
2022-05-11 06:55:44 +0100

    more

---
+++ src/components/More/MoreElements.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background: darkgrey;
+    color: #fff;
+
+    @media screen and (max-width: 768px) {
+        padding: 100px 0;
+    }
+`;
+
+export const SectionWrap = styled.div`
+
+`;
+
+export const SectionContent = styled.div`
+
+`;
+
+export const SectionContent2 = styled.div`
+
+`;
+
+export const SectionContent3 = styled.div`
+
+`;
+
+export const 
--- src/components/More/index.jsx
+++ src/components/More/index.jsx
@@ -6,16 +6,16 @@ const More = () => {
       <Container>
         <SectionWrap>
           <SectionContent>
-              <h2>Our Mission</h2>
-              <p>We create Personalized, customer-centric experiences, the right technology and people with our solutions deployed across various channels to suit the demography of the target market and analyze the data from Our two-way engagement with the market for the strategic use of clients.</p>
+              <heading>Our Mission</heading>
+              <paragraph>We create Personalized, customer-centric experiences, the right technology and people with our solutions deployed across various channels to suit the demography of the target market and analyze the data from Our two-way engagement with the market for the strategic use of clients.</paragraph>
           </SectionContent>
           <SectionContent2>
-              <h2>Execution</h2>
-              <p>A strong drive to see things through to completion as quickly, efficiently, and effectively as possible. We believe in the time to see projects through from start to finish in order to get exceptional results.</p>
+              <heading2>Execution</heading2>
+              <paragraph2>A strong drive to see things through to completion as quickly, efficiently, and effectively as possible. We believe in the time to see projects through from start to finish in order to get exceptional results.</paragraph2>
           </SectionContent2>
           <SectionContent3>
-              <h2>Enterprise</h2>
-              <p>Having the will and commitment to go to any length to complete a task. What differentiates us in a competitive business atmosphere is our individual and collective duty to own our goals and lead creative projects leading us to achieving them.</p>
+              <heading3>Enterprise</heading3>
+              <paragraph3>Having the will and commitment to go to any length to complete a task. What differentiates us in a competitive business atmosphere is our individual and collective duty to own our goals and lead creative projects leading us to achieving them.</paragraph3>
           </SectionContent3>
         </SectionWrap>
       </Container>
