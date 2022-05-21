bfc568ff4f3097ce38c284637f75e24ccb8b70cb
sibTunechy <ayotemitope930@gmail.com>
2022-05-21 07:25:10 +0100

    mm

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import JoinusPage from './pages/joinus';
 import Moreinfo from './pages/moreinfo';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 
 function App() {
+
+    const[isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <Router>
+            <Navbar toggle={toggle}/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/joinus' element={<JoinusPage/>}/>
                 <Route path='/moreinfo' element={<Moreinfo/>}/>
             </Routes>
+            <Footer/>
         </Router>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React, { useState} from 'react';
 import Sidebar from '../components/Sidebar'
-import Navbar from '../components/Navbar';
+// import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 import Services from '../components/Services';
 import { homeObjOne, homeObjThree, homeObjTwo } from '../components/InfoSection/Data';
-import Footer from '../components/Footer';
+// import Footer from '../components/Footer';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -17,13 +17,13 @@ const Home = () => {
   return (
     <>
         <Sidebar isOpen={isOpen} toggle={toggle}/>
-        <Navbar toggle={toggle}/>
+        {/* <Navbar toggle={toggle}/> */}
         <HeroSection/>
         <InfoSection {...homeObjOne}/> 
         <InfoSection {...homeObjTwo}/>
         <Services/>
         <InfoSection {...homeObjThree}/>
-        <Footer/> 
+        {/* <Footer/>  */}
     </>
   )
 }
