c062489201ad598c4fb7ecd1b4ada60073b5da23
sibTunechy <ayotemitope930@gmail.com>
2022-05-23 12:21:14 +0100

    mo

--- src/components/Aboutus/index.jsx
+++ src/components/Aboutus/index.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Aboutus() {
   return (
-    <div>Aboutus</div>
+    <div className='about-container'>
+        <h1>Aboutus</h1>
+        <div className='mission-container'>
+            <h2>Our Mission</h2>
+            <p>We create personalized, customer-centric experiences, the right technology and people with our solutions deployed across various channels to suit the demography of the target market and analyze the data from our two-way engagement with the market for the strategic use of clients.</p>
+        </div>
+        <div className='execution-contasiner'>
+            <h2>Execution</h2>
+            <p>A strong drive to see things through to completion as quickly, efficiently, and effectively as possible. We believe in the time to see projects through from start to finish in order to get exceptional results.</p>
+        </div>
+        <div className='enterprise-container'>
+            <h2>Enterprise</h2>
+            <p>Having the will and commitment to go to any length to complete a task. What differentiates us in a competitive business atmosphere is our individual and collective duty to own our goals and lead creative projects leading us to achieving them.</p>
+        </div>
+    </div>
   )
 }
 
