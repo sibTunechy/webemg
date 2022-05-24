fa78cb9891c91484edbaa2da196f3d630c8ab773
sibTunechy <ayotemitope930@gmail.com>
2022-05-24 07:01:57 +0100

    mm

--- src/components/Aboutus/index.jsx
+++ src/components/Aboutus/index.jsx
@@ -6,15 +6,15 @@ function Aboutus() {
     <div className='about-container'>
         <h1>About Us</h1>
         <div className='mission-container'>
-            <h2>Our Mission</h2>
+            <h2>OUR MISSION</h2>
             <p>We create personalized, customer-centric experiences, the right technology and people with our solutions deployed across various channels to suit the demography of the target market and analyze the data from our two-way engagement with the market for the strategic use of clients.</p>
         </div>
         <div className='execution-container'>
-            <h2>Execution</h2>
+            <h2>EXECUTION</h2>
             <p>A strong drive to see things through to completion as quickly, efficiently, and effectively as possible. We believe in the time to see projects through from start to finish in order to get exceptional results.</p>
         </div>
         <div className='enterprise-container'>
-            <h2>Enterprise</h2>
+            <h2>ENTERPRISE</h2>
             <p>Having the will and commitment to go to any length to complete a task. What differentiates us in a competitive business atmosphere is our individual and collective duty to own our goals and lead creative projects leading us to achieving them.</p>
         </div>
     </div>
