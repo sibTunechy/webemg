d74d53dc52966b8e26d6583c578c919f1e0c68c5
sibTunechy <ayotemitope930@gmail.com>
2022-05-26 08:57:18 +0100

    mm

---
+++ src/components/DigitalPage/Data.jsx
@@ -0,0 +1,15 @@
+export const digital = {
+    id: 'digital',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'DIGITAL TECHNOLOGY',
+    headline: 'Advanced Agile and other SDLC (Software Development Life Cycle) Methodologies, Cyber Security, Cloud Computing, Design thinking/Product Development Labs, and others are among our digital technology strengths.',
+    description: 'With SORO, we are championing new ways for governments and public servants to communicate with the general public, one personal text at a time. Platform as a Service (PaaS) and Software as a Servive (SaaS) are scalable solutions for businesses of all sizes. Our technologies have ushered new era of automation and great customer service where customer is king and the most innovative way wins.',
+    imgStart: 'true',
+    img: require('../../images/PlantBulb.jpg'),
+    alt: 'People',
+    dark: true,
+    primary: true,
+    darkText: false
+};
