ff12565f86ae1828e33d2c0d86ede44da836b741
sibTunechy <ayotemitope930@gmail.com>
2022-05-26 21:44:46 +0100

    mm

--- src/components/DigitalPage/DigitalElements.jsx
+++ src/components/DigitalPage/DigitalElements.jsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
+import img from '../../images/Blacksetting.jpg';
 
 export const DigitalContainer = styled.div`
     color: #fff;
-    background-image: url('../../images/PlantBulb.jpg');
-    background-color: green;
+    background-image: url('${img}');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    // background-color: green;
+
     // make image the background//
     
 
@@ -16,13 +21,13 @@ export const DigitalWrap = styled.div`
     display: grid;
     z-index: 1;
     height: 860px;
-    width: 100%;
-    max-width: 1100px;
+    width: 600px;
+    max-width: 1500px;
     margin-right: auto;
     margin-left: auto;
     padding: 0 24px;
     justify-content: center;
-    // border: 5px solid red;
+    border: 5px solid red;
 `;
 
 export const DigitalRows = styled.div`
@@ -42,11 +47,11 @@ export const Column1 = styled.div`
     grid-area: col1;
 `;
 
-export const Column2 = styled.div`
-    margin-bottom: 15px;
-    padding: 0 15px;
-    grid-area: col2;
-`;
+// export const Column2 = styled.div`
+//     margin-bottom: 15px;
+//     padding: 0 15px;
+//     grid-area: col2;
+// `;
 
 export const DigitalText = styled.div`
     max-width: 540px;
@@ -64,12 +69,19 @@ export const TopLine = styled.div`
     margin-bottom: 16px;
 `;
 
-export const Heading = styled.h1`
-    margin-bottom: 24px;
-    font-size: 28px;
-    line-height: 1.3;
+export const Heading = styled.p`
+    // font-family: Georgia, 'Times New Roman', Times, serif;
+    // margin-top: 24px;
+
+    // align text here
+
+    text-align: center;
+    font-size: 24px;
+    max-width: 900px;
+    line-height: 180%;
+    text-shadow: 1px -1px blue;
     font-weight: 600;
-    color: ${({lightText}) => (lightText ? 'black' : '#010606')};
+    color: ${({lightText}) => (lightText ? 'white' : 'white')};
 
     @media screen and (max-width : 480px) {
         font-size: 32px;
@@ -77,20 +89,25 @@ export const Heading = styled.h1`
 `;
 
 export const Subtitle = styled.p`
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    text-shadow: 1px 1px black;
     max-width: 440px;
     margin-bottom: 35px;
-    font-size: 18px;
-    line-height: 24px;
-    color: ${({darkText}) => (darkText ? 'black' : 'black')};
+    font-size: 28px;
+    line-height: 140%;
+    // justify-content: center;
+    // align-items: center;
+    // align-text: center;
+    color: ${({darkText}) => (darkText ? 'white' : 'white')};
 `;
 
-export const DigitalImage = styled.div`
-    max-width: 555px;
-    height: 100%;
-`;
+// export const DigitalImage = styled.div`
+//     max-width: 555px;
+//     height: 100%;
+// `;
 
-export const Img = styled.img`
-    width: 100%;
-    margin: 0 0 10px 0;
-    padding-right: 0;
-`;
+// export const Img = styled.img`
+//     width: 100%;
+//     margin: 0 0 10px 0;
+//     padding-right: 0;
+// `;
--- src/components/DigitalPage/index.jsx
+++ src/components/DigitalPage/index.jsx
@@ -6,20 +6,20 @@ function Digital({lightBg, id, topLine, imgStart, lightText, darkText, headline,
     <>
         <DigitalContainer lightBg={lightBg} id={id}>
             <DigitalWrap>
-                <DigitalRows imgStart={imgStart}>
+                {/* <DigitalRows imgStart={imgStart}> */}
                     <Column1>
                         <DigitalText>
                             <TopLine>DIGITAL TECHNOLOGY</TopLine>
-                            <Heading  lightText={lightText}>Advanced Agile and other SDLC (Software Development Life Cycle) Methodologies, Cyber Security, Cloud Computing, Design thinking/Product Development Labs, and others are among our digital technology strengths.</Heading>
-                            <Subtitle darkText={darkText}>With SORO, we are championing new ways for governments and public servants to communicate with the general public, one personal text at a time. Platform as a Service (PaaS) and Software as a Servive (SaaS) are scalable solutions for businesses of all sizes. Our technologies have ushered new era of automation and great customer service where customer is king and the most innovative way wins.</Subtitle>
+                            <Heading  lightText={lightText}>Advanced Agile and other SDLC (Software Development Life Cycle) Methodologies, Cyber Security, Cloud Computing, Design thinking/Product Development Labs, and others are among our digital technology strengths. With SORO, we are championing new ways for governments and public servants to communicate with the general public, one personal text at a time. Platform as a Service (PaaS) and Software as a Servive (SaaS) are scalable solutions for businesses of all sizes. Our technologies have ushered new era of automation and great customer service where customer is king and the most innovative way wins.</Heading>
+                            {/* <Subtitle darkText={darkText}>With SORO, we are championing new ways for governments and public servants to communicate with the general public, one personal text at a time. Platform as a Service (PaaS) and Software as a Servive (SaaS) are scalable solutions for businesses of all sizes. Our technologies have ushered new era of automation and great customer service where customer is king and the most innovative way wins.</Subtitle> */}
                         </DigitalText>
                     </Column1>
-                    <Column2>
+                    {/* <Column2>
                         <DigitalImage>
                             <Img src={img} alt={alt}/>
                         </DigitalImage>
-                    </Column2>
-                </DigitalRows>
+                    </Column2> */}
+                {/* </DigitalRows> */}
             </DigitalWrap>
         </DigitalContainer>
     </>
