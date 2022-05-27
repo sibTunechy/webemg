fa443b9c99ebe7b2b9b7c90e0edaca09a407b03a
sibTunechy <ayotemitope930@gmail.com>
2022-05-27 09:08:57 +0100

    m

--- src/components/DigitalPage/index.jsx
+++ src/components/DigitalPage/index.jsx
@@ -7,7 +7,7 @@ function Digital() {
         <DigitalContainer>
             <DigitalWrap>          
                 <TopLine>Digital Technology</TopLine>
-                <Heading  >Advanced Agile and other SDLC (Software Development Life Cycle) Methodologies, Cyber Security, Cloud Computing, Design thinking/Product Development Labs, and others are among our digital technology strengths. With SORO, we are championing new ways for governments and public servants to communicate with the general public, one personal text at a time. Platform as a Service (PaaS) and Software as a Servive (SaaS) are scalable solutions for businesses of all sizes. Our technologies have ushered new era of automation and great customer service where customer is king and the most innovative way wins.</Heading>
+                <Heading>Advanced Agile and other SDLC (Software Development Life Cycle) Methodologies, Cyber Security, Cloud Computing, Design thinking/Product Development Labs, and others are among our digital technology strengths. With SORO, we are championing new ways for governments and public servants to communicate with the general public, one personal text at a time. Platform as a Service (PaaS) and Software as a Servive (SaaS) are scalable solutions for businesses of all sizes. Our technologies have ushered new era of automation and great customer service where customer is king and the most innovative way wins.</Heading>
             </DigitalWrap>
         </DigitalContainer>
     </>
---
+++ src/components/Fleet/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Fleet() {
+  return (
+    <>
+        <FleetContainer>
+            <FleetWrap>
+                <FleetHead>FLEET MANAGEMENT</FleetHead>
+                <Fleetdetails>Whether it's a financial lease, an operations lease, or any other lease solution, we build up structures and controls to run fleet operations and maximize profit. We handle operations
+                    for your fleet using our innovative and professional employees, advanced technology, and pool of knowlledge. To decrease theft and loss and improve operational efficiency, we identify, track and monitor
+                    the condition of your cars in real time.
+                    
+                </Fleetdetails>
+            </FleetWrap>
+        </FleetContainer>
+    </>
+  )
+}
+
+export default Fleet;
