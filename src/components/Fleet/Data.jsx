f394c76d20ed60be5c28b21cd4870eb3cb720775
sibTunechy <ayotemitope930@gmail.com>
2022-05-27 19:37:57 +0100

    mm

---
+++ src/components/Fleet/Data.jsx
@@ -0,0 +1,12 @@
+export const fleetsObj = {
+    id: 'fleet',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    imgStart: false,
+    img: require('../../images/housecars.png'),
+    alt: 'cars',
+    dark: true,
+    primary: true,
+    darkText: false
+};
---
+++ src/components/Fleet/FleetElements.jsx
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+export const FleetContainer = styled.div`
+    color: #fff;
+    background: ${({lightBg}) => (lightBg ? '#f9f9f9' : 'white')};
+
+    @media screen and (max-width) {
+        padding: 100px 0;
+    }
+`;
+
+export const FleetWrap = styled.div`
+    display: grid;
+    z-index: 1;
+    height: 860px;
+    width: 100%;
+    max-width: 1100px;
+    margin-right: auto;
+    margin-left: auto;
+    padding: 0 24px;
+    justify-content: center;
+    border: 3px solid red;
+`;
+
+export const FleetRows = styled.div`
+    display: grid;
+    grid-auto-columns: minmax(auto, 1fr);
+    align-items: center;
+    grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'`: `'col1 col2'`)};
+
+    @media screen and (max-width: 768px) {
+        grid-template-areas: ${({imgStart}) => (imgStart ? `'col1' 'col2'`: `'col1 col1' 'col2 col2'`)};
+    }
+`;
+
+export const Column1 = styled.div`
+    margin-bottom: 15px;
+    padding: 0 15px;
+    grid-area: col1;
+    border: 2px solid red;
+`;
+
+export const Column2 = styled.div`
+    max-width: 540px;
+    padding-top: 0;
+    padding-bottom: 60px;
+    border: 2px solid red;
+`;
+
+export const FleetHead = styled.p`
+    color: #01bf71;
+    font-size: 25px;
+    line-height: 16px;
+    font-weight: 700;
+    letter-spacing: 1.4px;
+    text-transform: uppercase;
+    margin-bottom: 16px;
+`;
+
+export const FleetDetails = styled.p`
+    margin-bottom: 24px;
+    font-size: 28px;
+    line-height: 1.3;
+    font-weight: 600;
+    color: ${({lightText}) => (lightText ? 'black' : '#010606')};
+
+    @media screen and (max-width : 480px) {
+        font-size: 32px;
+    }
+`;
+
+export const ImgWrap = styled.div`
+    max-width: 555px;
+    height: 100%;
+    border: 2px solid green;
+`;
+
+export const Img = styled.img`
+    width: 100%;
+    margin: 0 0 10px 0;
+    padding-right: 0;
+`;
