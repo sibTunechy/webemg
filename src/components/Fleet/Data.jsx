f218ad6f1fe193811b8a44399d3c6a2fa7978f23
sibTunechy <ayotemitope930@gmail.com>
2022-05-29 20:11:25 +0100

    mm

--- src/components/Fleet/Data.jsx
+++ src/components/Fleet/Data.jsx
@@ -4,7 +4,7 @@ export const fleetsObj = {
     lightText: true,
     lightTextDesc: true,
     topLine: 'FLEET MANAGEMENT',
-    headline: '',
+    headline: ' Whether it is a financial lease, an operations lease, or any other lease solution, we build up structures and controls to run fleet operations and maximize profit. We handle operations for your fleet using our innovative and professional employees, advanced technology, and pool of knowledge. To decrease theft and loss and improve operational efficiency, we identify, track and monitor the condition of your cars in real time.',
     description: '',
     imgStart: true,
     img: require('../../images/svg-2.png'),
