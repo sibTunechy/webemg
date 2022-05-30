8c2027618f0e95dda4641f5c470d4a5cc6523199
sibTunechy <ayotemitope930@gmail.com>
2022-05-30 08:21:01 +0100

    mm

--- src/components/Fleet/index.jsx
+++ src/components/Fleet/index.jsx
@@ -7,10 +7,15 @@ const Fleets = () => {
     <>
         <div className='fleet-container'>
             <div className='fleet-column' >
-
             </div>
             <div className='fleet-column2'>
-                
+                {/* <section> */}
+                  <h1>FLEET MANAGEMENT</h1>
+                  <p>
+                  Whether it is a financial lease, an operations lease, or any other lease solution, we build up structures and controls to run fleet operations and maximize profit. We handle operations for your fleet using our innovative and professional employees, advanced technology, and pool of knowledge. To decrease theft and loss and improve operational efficiency, we identify, track and monitor the condition of your cars in real time.    
+                  Depending on your specific needs, we provide a variety of tracking solutions. Our solutions will provide you a clear, thorough picyure of your fleet, reducing delays and expenses. We provide necessary products and services to help you manage your fleet effectively and efficiently.
+                  </p>
+                {/* </section> */}
             </div>
         </div>
     </>
