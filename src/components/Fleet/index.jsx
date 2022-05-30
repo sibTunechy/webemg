5234b6e4a0c5ea7bba9adcce3e68adb15b87ed67
sibTunechy <ayotemitope930@gmail.com>
2022-05-30 07:35:22 +0100

    mm

--- src/components/Fleet/index.jsx
+++ src/components/Fleet/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Fleetsimg from '../../images/housecars.png';
+// import Fleetsimg from '../../images/stepbulb.jpg';
 import '../../styles/Fleets.css';
 
 const Fleets = () => {
   return (
     <>
         <div className='fleet-container'>
-            <div className='fleet-column'>
+            <div className='fleet-column' >
 
             </div>
             <div className='fleet-column2'>
