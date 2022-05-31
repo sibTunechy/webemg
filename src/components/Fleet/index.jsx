3828fd1b9c96b8519d1409589268ae0f35c32ddd
sibTunechy <ayotemitope930@gmail.com>
2022-05-31 09:01:19 +0100

    mm

--- src/components/Fleet/index.jsx
+++ src/components/Fleet/index.jsx
@@ -6,15 +6,17 @@ const Fleets = () => {
   return (
     <>
         <div className='fleet-container'>
-            <div className='fleet-column' >
-            </div>
-            <div className='fleet-column2'>      
-                  <h1>FLEET MANAGEMENT</h1>
-                  <p>
-                  Whether it is a financial lease, an operations lease, or any other lease solution, we build up structures and controls to run fleet operations and maximize profit. We handle operations for your fleet using our innovative and professional employees, advanced technology, and pool of knowledge. To decrease theft and loss and improve operational efficiency, we identify, track and monitor the condition of your cars in real time.    
-                  Depending on your specific needs, we provide a variety of tracking solutions. Our solutions will provide you a clear, thorough picture of your fleet, reducing delays and expenses. We provide necessary products and services to help you manage your fleet effectively and efficiently.
-                  </p>
-            </div>
+            <div className='contain'>
+                <div className='fleet-column' >
+                </div>
+                <div className='fleet-column2'>      
+                      <h1>FLEET MANAGEMENT</h1>
+                      <p>
+                      Whether it is a financial lease, an operations lease, or any other lease solution, we build up structures and controls to run fleet operations and maximize profit. We handle operations for your fleet using our innovative and professional employees, advanced technology, and pool of knowledge. To decrease theft and loss and improve operational efficiency, we identify, track and monitor the condition of your cars in real time.    
+                      Depending on your specific needs, we provide a variety of tracking solutions. Our solutions will provide you a clear, thorough picture of your fleet, reducing delays and expenses. We provide necessary products and services to help you manage your fleet effectively and efficiently.
+                      </p>
+                </div>
+            </div>  
         </div>
     </>
   )
