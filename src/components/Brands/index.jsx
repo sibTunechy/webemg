7b5ccb09e9d850311a7fdb07247cda17aa9ff2ca
sibTunechy <ayotemitope930@gmail.com>
2022-06-01 05:54:30 +0100

    mm

--- src/components/Brands/index.jsx
+++ src/components/Brands/index.jsx
@@ -5,7 +5,7 @@ function Brand() {
     <>
         <div className='brand-container'>
             <div className='brand-wrap'>
-                <h1>Brand Activation</h1>
+                <h1>BRAND ACTIVATION</h1>
                 <p>We provide a memorable, dynamic brand experience whenever a customer engages with our client's based in Nigeria, ensuring brand affinity, loyalty, and sales growth. Our event-based marketing tactics are well thought out and deployed in order to have a favorable impact on the customer's purchasing patterns
                 We use a tried-and-true strategy for evaluating experential programs, combining qualitative and quantitative techniques to determine the quantity of business potential created, as well as the influence on brand affinity, relationship impact, and experience quality.
                 </p>
