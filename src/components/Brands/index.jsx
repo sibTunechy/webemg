b09fbba6215ecb077de8afced0f00228dc9ad30c
sibTunechy <ayotemitope930@gmail.com>
2022-06-01 22:30:09 +0100

    mm

--- src/components/Brands/index.jsx
+++ src/components/Brands/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import '../../styles/Brand.css';
+import '../../styles/Brand.css';
 
 const Brand= () => {
   return (
     <>
-        <div className='brand-container'>
+        <div className='brand-contaner'>
             <div className='brand-wrap'>
-                <div className='details'>
+                <div className='brand-details'>
                   <h1>BRAND ACTIVATION</h1>
                   <p>  We provide a memorable, dynamic brand experience whenever a customer engages with our clients based in Nigeria, ensuring brand affinity, loyalty, and sales growth. Our event-based marketing tactics are well thought out and deployed in order to have a favorable impact on the customers purchasing patterns. We use a tried-and-true strategy for evaluating experential programs, combining qualitative and quantitative techniques to determine the quantity of business potential created, as well as the influence on brand affinity, relationship impact, and experience quality. </p>
                 </div>
