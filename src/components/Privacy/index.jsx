2639bb691578a161cba007ed418af696a54c05f7
sibTunechy <ayotemitope930@gmail.com>
2022-06-02 09:23:10 +0100

    mm

---
+++ src/components/Privacy/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Privacy = () => {
+  return (
+    <>
+        <div className='privacy-container'>
+            <div classname='privacy-wrap'>
+                <div className='privacy-head'>
+                    <h1>Privacy Policy Notice</h1>
+                </div>
+                <p> We have made updates to our Privacy Policy to better protect your personal data. Please take a look at our updated Privacy Policy.
+                    I agree to the updated Privacy Policy. I understand the use of ,y personal data by EMG will be in accordance with the updated Privacy Policy.
+                </p>
+            </div>
+
+        </div>
+    </>
+  )
+}
+
+export default Privacy;
