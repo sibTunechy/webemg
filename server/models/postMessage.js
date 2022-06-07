c49d54de1edb5aaf65941b2bd5c711f9dc747ec4
sibTunechy <ayotemitope930@gmail.com>
2022-06-07 08:39:19 +0100

    mm

---
+++ server/models/postMessage.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    }
+});
+
+const PostMessage = mongoose.model('PostMessage', postSchema)
+
+export default PostMessage;
