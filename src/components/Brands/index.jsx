d98bff04f94b010442155702b207a7bae46eac3e
sibTunechy <ayotemitope930@gmail.com>
2022-06-10 17:53:42 +0100

    packagejson frontend check

--- src/components/Brands/index.jsx
+++ src/components/Brands/index.jsx
@@ -10,7 +10,9 @@ const Brand= () => {
                   <p>  We provide a memorable, dynamic brand experience whenever a customer engages with our clients based in Nigeria, ensuring brand affinity, loyalty, and sales growth. Our event-based marketing tactics are well thought out and deployed in order to have a favorable impact on the customers purchasing patterns. We use a tried-and-true strategy for evaluating experential programs, combining qualitative and quantitative techniques to determine the quantity of business potential created, as well as the influence on brand affinity, relationship impact, and experience quality. </p>
             </div>
         </div>
-           
+      <div>
+        <h1></h1>
+      </div>
     </>
   )
 };
