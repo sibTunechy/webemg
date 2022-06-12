7545e27062a92996489ff2944c4532038f032930
sibTunechy <ayotemitope930@gmail.com>
2022-06-12 17:56:17 +0100

    mm

--- src/components/Auth/styles.js
+++ src/components/Auth/styles.js
@@ -1,1 +1,20 @@
 import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: theme.spacing(2),
+    },
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+        },
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+})
