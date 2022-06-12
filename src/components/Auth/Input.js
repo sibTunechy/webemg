e3452716bc4810d8e2827d852f04ff93f2ef9dc2
sibTunechy <ayotemitope930@gmail.com>
2022-06-12 18:47:39 +0100

    mm

--- src/components/Auth/Input.js
+++ src/components/Auth/Input.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
 
-const Input = () => {
+const Input = ({ half }) => {
   return (
-    <div>Input</div>
+    <Grid item xs={12} sm={half ? 6 : 12}>
+        <TextField
+            name={name}
+            onChange={handleChange}
+            variant='outlined'
+            required
+            fullWidth
+            label={label}
+            autofocus={autoFocus}
+            type={type}
+            InputProps={name === 'password' ? {
+                endAdornment: (
+                    <InputAdornment>
+                        <IconButton onClick={handdleShowPassword}>
+                            {type === 'password' ? <Visisbilty/> : <VisibiltyOff/> }
+                        </IconButton>
+                    </InputAdornment>
+                )
+            }}
+        />
+    </Grid>
   )
 }
 
