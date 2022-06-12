a8d36b451db4d28b06187c92b5fcfdb6dc99725a
sibTunechy <ayotemitope930@gmail.com>
2022-06-12 13:28:34 +0100

    mm

--- src/components/Signin/index.jsx
+++ src/components/Signin/index.jsx
@@ -4,21 +4,30 @@ import { FormLabel, Container, FormWrap, Icon, FormContent, Form, FormH1, FormIn
 const Signin = () => {
     const isSignup = false;
 
+    const handleSubmit = () => {
+
+    }
+
   return (
     <>
         <Container>
             <FormWrap>
                 <Icon to='/'>EMG</Icon>
-                <FormContent>
-                    <Form action='#'>
-                        <FormH1>{isSignup ? 'Sign Up' : 'Sign In'}</FormH1>
-                        <FormLabel htmlFor='for'>Email</FormLabel>
-                        <FormInput type='email' required/>
-                        <FormLabel htmlFor='for'>Password</FormLabel>
-                        <FormInput type='password' required />
-                        <FormButton type='submit'>Continue</FormButton>
-                        <Text>Forget password</Text>
-                    </Form>
+                <FormContent> 
+                    {isSignup && (
+                        <>
+                        <Form action='#'>
+                            <FormH1>{isSignup ? 'Sign Up' : 'Sign In'}</FormH1>
+                            <FormLabel htmlFor='for'>Email</FormLabel>
+                            <FormInput type='email' required/>
+                            <FormLabel htmlFor='for'>Password</FormLabel>
+                            <FormInput type='password' required />
+                            <FormButton type='submit' onSubmit={handleSubmit}>Continue</FormButton>
+                            <Text>Forget password</Text>
+                        </Form>
+                        </>
+                    )
+                    }
                 </FormContent>
             </FormWrap>
         </Container>
