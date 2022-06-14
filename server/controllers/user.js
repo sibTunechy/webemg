e5f6e3236cfffc5b1568fe1935c19d3b732ea19f
sibTunechy <ayotemitope930@gmail.com>
2022-06-14 08:29:16 +0100

    mm

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,12 @@
 import bcrypt from 'bycryptjs';
 import jwt from 'jsonwebtoken';
 
+import User from '../models/user.js';
+
+export const signin = async (res, req) => {
+
+};
+
+export const signup = async (res, req) => {
+
+};
