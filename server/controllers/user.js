082c771af698f9c54208d3fa36274f38856d2014
sibTunechy <ayotemitope930@gmail.com>
2022-06-14 08:40:40 +0100

    mm

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,10 +1,25 @@
+import { Email } from '@material-ui/icons';
 import bcrypt from 'bycryptjs';
 import jwt from 'jsonwebtoken';
 
 import User from '../models/user.js';
 
 export const signin = async (res, req) => {
+    const { email, password } = req.body;
 
+    try {
+        const existingUser = await User.fimdOne({email});
+
+        if(!existingUser) return res.status(404).json({message: 'User does not exist.'});
+
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+
+        if(!isPasswordCorrect) return res.status(404).json({message: 'Invalid credentials'});
+
+        
+    } catch (error) {
+        
+    }
 };
 
 export const signup = async (res, req) => {
