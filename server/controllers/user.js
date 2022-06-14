2510554d2e177cff10b2d1c1760b0a69558579ea
sibTunechy <ayotemitope930@gmail.com>
2022-06-14 09:08:59 +0100

    mm

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 
 import User from '../models/user.js';
 
-export const signin = async (res, req) => {
+export const signin = async (req, res) => {
     const { email, password } = req.body;
 
     try {
@@ -23,6 +23,25 @@ export const signin = async (res, req) => {
     }
 };
 
-export const signup = async (res, req) => {
+export const signup = async (req, res) => {
+    const { email, password, confirmPassword, firstName, lastName } = req.body
 
+    try {
+        const existingUser = await User.findOne({email});
+
+        if(existingUser) return res.status(400).json({message: 'User already exists'});
+
+        if(password !== confirmPassword) return res.status(400)({message: 'Password do not match'});
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const result = await User.create({email, password: hashedPassword, name: `${firstName} ${lastName}`});
+
+        const token = jwt.sign({email: result.email, id: result._id}, 'test', {expiresIn: '1hr'});
+
+        res.status(200).json({result, token});
+    } catch (error) {
+        res.status(500).json({message: 'Something went wrong'});
+        
+    }
 };
