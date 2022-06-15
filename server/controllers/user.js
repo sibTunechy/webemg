f4a37b0892d48d04fc6493391ffa76b2a42a98f4
sibTunechy <ayotemitope930@gmail.com>
2022-06-15 08:17:27 +0100

    mm

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -7,7 +7,7 @@ export const signin = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const existingUser = await User.fimdOne({email});
+        const existingUser = await User.findOne({email});
 
         if(!existingUser) return res.status(404).json({message: 'User does not exist.'});
 
--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,25 +1,25 @@
 import { AUTH } from '../constants/actionTypes';
 import * as api from '../api/index.js';
 
-export const signin = (formData, history) => async (dispatch) => {
+export const signin = (formData, navigate) => async (dispatch) => {
     try {
         const {data} = await api.signIn(formData);
         
         dispatch({type: AUTH, data});
         // log in the user...
-        history.push('/');
+        navigate.push('/');
     } catch (error) {
-        console.log(error);
+        console.log(error.response.data);
     }
 };
 
-export const signup = (formData, history) => async (dispatch) => {
+export const signup = (formData, navigate) => async (dispatch) => {
     try {
         const {data} = await api.signUp(formData);
 
         dispatch({type: AUTH, data});
         // sign up the user..
-        history.push('/');
+        navigate.push('/');
     } catch (error) {
         console.log(error);
     }
