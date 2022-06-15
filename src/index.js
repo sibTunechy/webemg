1bdbe4f89d3bbc610951f342cf69877ec234a784
sibTunechy <ayotemitope930@gmail.com>
2022-06-15 06:30:30 +0100

    mm

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-
 import App from './App'
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>,
     document.getElementById('root')
 );
 
