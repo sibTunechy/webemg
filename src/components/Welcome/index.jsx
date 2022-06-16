f2997f54f23f84d2453166062696ff7b639ac0f1
sibTunechy <ayotemitope930@gmail.com>
2022-06-16 07:00:31 +0100

    mm

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import  '../../styles/Welcome.css';
 
 const welcome = () => {
   return (
     <>
         <div className='welcome-container'>
-            
+            <h1>Welcome to EMG</h1>
         </div>
     </>
   )
