adf28ee73242a67ab13ac4ab9759d8b9dac2b135
sibTunechy <ayotemitope930@gmail.com>
2022-06-16 06:57:17 +0100

    mm

---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const welcome = () => {
+  return (
+    <>
+        <div className='welcome-container'>
+            
+        </div>
+    </>
+  )
+}
+
+export default welcome;
