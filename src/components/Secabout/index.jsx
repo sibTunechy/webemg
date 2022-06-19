806d8056e81c9180dc722d02e9ab5ba5e54ac940
sibTunechy <ayotemitope930@gmail.com>
2022-06-19 06:47:08 +0100

    mm

--- src/components/Secabout/index.jsx
+++ src/components/Secabout/index.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Secabout = () => {
   return (
-    <div>Secabout</div>
+    <>
+        <div className='first-section'>
+            <div className=''>
+                <div className=''>
+
+                </div>
+
+                <div>
+
+                </div>
+            </div>
+        </div>
+
+        <div classname='second-section'>
+
+        </div>
+
+        <div className='third-section'>
+
+        </div>
+
+        <div classname='fourth-section'> 
+
+        </div>
+    </>
   )
 }
 
