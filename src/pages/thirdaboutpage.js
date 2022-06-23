9d0856b40ee43e196bcdd5d65cc83e3e7e14b1b4
sibTunechy <ayotemitope930@gmail.com>
2022-06-23 12:10:41 +0100

    mm

---
+++ src/pages/thirdaboutpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Thirdabout from '../components/Thirdabout';
+
+
+const thirdaboutpage = () => {
+  return (
+    <>
+        <Thirdabout/>
+    </>
+  )
+}
+
+export default thirdaboutpage
