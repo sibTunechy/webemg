7a641208aa1bf61cc7e8cbe87009a671e2b8fc77
sibTunechy <ayotemitope930@gmail.com>
2022-06-23 11:34:36 +0100

    mm

--- src/components/Thirdabout/index.jsx
+++ src/components/Thirdabout/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
-const index = () => {
+const Thirdabout = () => {
   return (
-    <div>index</div>
+    <>
+        <div className='row1-container'>
+            <div className='box box-down cyan'>
+                <h2>Our Mission</h2>
+                <p>We create personalized, customer-centric experiences, the right technology and people with our
+                    solutions deployed across various channels to suit the demography of the target market and analyze
+                    the data from Our two-way engagement with the market for strategic use of clients.
+                    <img alt=''/>
+                </p>
+            </div>
+
+
+            <div className='box red'>
+                <h2>Excellence Statement</h2>
+                <p>Consistently delivering exceptional results Every employee's heart beat with the phrase of <em>Excellence</em>.
+                    To provide measureable value to ur stakholders, we go above and beyond the ordinary.
+                </p>
+            </div>
+        </div> 
+    </>
   )
 }
 
-export default index
+export default Thirdabout;
