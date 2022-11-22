e94b236c91e04c82cbc17ef4334cba73e484c762
Ahmed Abdullahi <86810831+sibTunechy@users.noreply.github.com>
2022-11-22 17:21:33 +0100

    m

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -130,7 +130,7 @@ const handleLogout = ()=>{
                 exact="true"
                 offset={-80}
               >
-                
+                Discover
               </NavLinks>
             </NavItem>
           </NavMenu>
