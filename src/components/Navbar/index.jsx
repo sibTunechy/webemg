83c134d59ea92e9ad829f286e4854b8dee1195ad
Ahmed Abdullahi <86810831+sibTunechy@users.noreply.github.com>
2022-11-27 22:31:19 +0100

    m

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -73,7 +73,7 @@ const handleLogout = ()=>{
   return (
     <>
       <Nav scrollNav={scrollNav}>
-        <NavbarCont>
+        <NavbarContainer>
           <NavLogo to="/" onClick={toggleHome} >
            <img src={myLogo} alt='myLogo' />
           </NavLogo>
@@ -143,7 +143,7 @@ const handleLogout = ()=>{
             )}
             {/* <NavBtnLink to='/auth'>Join Us</NavBtnLink> */}
           </NavBtn>
-        </NavbarCont>
+        </NavbarContainer>
       </Nav>
     </>
   );
