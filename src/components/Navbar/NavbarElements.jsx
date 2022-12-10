52ff9acb8d6f064b9979da65cff0dc71a34f773d
Ahmed Abdullahi <86810831+sibTunechy@users.noreply.github.com>
2022-12-10 21:30:15 +0100

    m

--- src/components/Navbar/NavbarElements.jsx
+++ src/components/Navbar/NavbarElements.jsx
@@ -79,7 +79,7 @@ export const NavMenu = styled.ul`
     align-items: center;
     list-style: none;
     text-align: center;
-    margin-right: -22px;
+    margin-ri: -22px;
 
     @media screen and (max-width: 768px) {
         display: none;
