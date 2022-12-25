a78ebd66ce29849d3e11ba45bc7a5d91f2c2f813
Ahmed Abdullahi <86810831+sibTunechy@users.noreply.github.com>
2022-12-25 18:01:52 +0100

    m

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -108,7 +108,7 @@ const handleLogout = ()=>{
                 exact="true"
                 offset={-80}
               >
-                Services
+                Servic
               </NavLinks>
             </NavItem>
             <NavItem>
