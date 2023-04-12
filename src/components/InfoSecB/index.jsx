4475a3d12fe25a69076d120b04bee644a7fb1eed
Ahmed Abdullahi <86810831+sibTunechy@users.noreply.github.com>
2023-04-12 17:04:00 +0100

    new page changes

--- src/components/InfoSecB/index.jsx
+++ src/components/InfoSecB/index.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Infosec = () => {
   return (
-    <>
-
+    <>  
+    <div>
+      <h1>Digital Technology</h1>
+      <span>Advanced Agile and other SDLC (Software Development Life Cycle) Methodologies, Cyber Security, Cloud Computing, Design thinking/Product Development Labs</span>
+    </div>
+    <br></br>
+    <div>
+      <div>
+        <p></p>
+      </div>
+      <div>
+        <img/>
+      </div>
+    </div>     
     </>
   )
 }
