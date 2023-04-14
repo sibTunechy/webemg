6a57ff34f12d3420ed253ddd233552fe9b3070e4
Ahmed Abdullahi <86810831+sibTunechy@users.noreply.github.com>
2023-04-14 17:19:54 +0100

    new page

--- src/components/InfoSecB/index.jsx
+++ src/components/InfoSecB/index.jsx
@@ -10,12 +10,12 @@ const Infosec = () => {
       <span>Advanced Agile and other SDLC (Software Development Life Cycle) Methodologies, Cyber Security, Cloud Computing, Design thinking/Product Development Labs, and others are among our digital technology strengths.</span>
     </div>
     <br></br>
-    <div>
-      <div>
+    <div className='container-parent'>
+      <div className='div-info'>
         <h1>With SORO, we are championing new ways for governments and public servants to communicate with the general public, one personal text at a time.</h1>
         <p>Platform as a Service (PaaS) and Software as a Servive (SaaS) are scalable solutions for businesses of all sizes. Our technologies have ushered new era of automation and great customer service where customer is king and the most innovative way wins.</p>
       </div>
-      <div>
+      <div className='div-image'>
         <img alt='timegear' src={Icon4} width='80' height='150'/>
       </div>
     </div>     
